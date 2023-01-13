ba3d3e565526ef5641eaa47e4ee8b780f3788317
Azhar <abdazhar786@gmail.com>
2023-01-13 23:00:03 +0530

    Add animation on Blog

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useEffect}from 'react'
 import './blog.scss'
 import {BsArrowRightShort} from 'react-icons/bs'
 
@@ -6,6 +6,10 @@ import img3 from '../../Assets/image3.jpg'
 import img10 from '../../Assets/image10.jpg'
 import img12 from '../../Assets/image12.jpg'
 import img16 from '../../Assets/image16.jpg'
+
+import Aos from 'aos'
+import 'aos/dist/aos.css'
+
 
 const Posts =[
   {
@@ -35,15 +39,20 @@ const Posts =[
 ]
 
 const Blog = () => {
+
+  useEffect(()=>{
+    Aos.init({duration:2000})
+  }, [])
+
   return (
     <section className='blog container section'>
 
       <div className="secContainer">
         <div className="secIntro">
-          <h2 className='secTitle'>
+          <h2 data-aos="fade-up"  data-aos-duration="2000" className='secTitle'>
             Our Best Blog?
           </h2>
-          <p>
+          <p data-aos="fade-up"  data-aos-duration="2500">
             An insight to the incredible experience in the world.
           </p>
         </div>
@@ -52,18 +61,18 @@ const Blog = () => {
            {
             Posts.map(({id, postImage,title,desc})=>{
               return (
-                <div className="singlePost grid">
+                <div data-aos="fade-up"  data-aos-duration="2000" className="singlePost grid">
                 <div className="imgDiv">
                   <img src={postImage} alt={title} />
                 </div>
 
                 <div className="postDetails">
-                  <h3>
+                  <h3 data-aos="fade-up"  data-aos-duration="3000">
                     {title}
                   </h3>
-                  <p>{desc}</p>
+                  <p data-aos="fade-up"  data-aos-duration="4000">{desc}</p>
                 </div>
-                <a href="#" className="flex">
+                <a href="#" className="flex" data-aos="fade-up"  data-aos-duration="4500">
 
                   Read More
                   <BsArrowRightShort className="icon"/>
